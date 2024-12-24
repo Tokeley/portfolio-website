fa595b1c9439c340178e3d5c515ddac085c44eca
Alex Manning <alex.t.manning@gmail.com>
2024-12-24 15:46:38 +1300

    Added contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,13 +1,135 @@
-import { Heading } from '@chakra-ui/react';
+import { Input, Textarea, Button } from '@chakra-ui/react';
+import { RiMailLine, RiCheckLine } from "react-icons/ri";
+import emailjs from 'emailjs-com';
+import { useState } from 'react';
 
 const Contact = () => {
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    subject: '',
+    message: '',
+  });
+
+  const [submitted, setSubmitted] = useState(false); // Added state to track submission status
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const templateParams = {
+      firstName: formData.firstName,
+      lastName: formData.lastName,
+      email: formData.email,
+      subject: formData.subject,
+      message: formData.message,
+    };
+
+    emailjs.send(
+      'service_t1pv2nb',  
+      'template_tf9fich',  
+      templateParams,
+      'l24TP84Z8Ia6A2Kkh'
+    )
+    .then((response) => {
+      console.log('Email sent successfully:', response);
+      setSubmitted(true); 
+    })
+    .catch((error) => {
+      console.error('Error sending email:', error);
+    });
+
+    setFormData({
+      firstName: '',
+      lastName: '',
+      email: '',
+      subject: '',
+      message: '',
+    });
+  };
+
   return (
-    <section id="Contact" className="section h-screen flex items-center justify-center pt-14">
-      <div className="h-full w-2/3 flex items-center justify-center">
-        <Heading as="h2" size="2xl" className="text-gray-700">Contact</Heading>
+    <section id="Contact" className="section h-screen flex justify-center pt-14">
+      <div className="lg:w-2/3 w-10/12 justify-center p-0">
+        <h1 className='pb-6 lg:text-6xl text-4xl font-montserratMedium text-gray-700 dark:text-offwhite p-2 text-center'>
+          Contact
+        </h1>
+        <form onSubmit={handleSubmit}>
+          <div className="lg:flex lg:space-y-0 space-y-4">
+            <Input
+              name="firstName"
+              value={formData.firstName}
+              onChange={handleInputChange}
+              placeholder="First Name"
+              variant="outline"
+              className='font-montserratMedium w-full lg:mr-2 border-2 dark:border-gray-800 px-2 bg-white/70 dark:bg-gray-700/70'
+            />
+            <Input
+              name="lastName"
+              value={formData.lastName}
+              onChange={handleInputChange}
+              placeholder="Last Name"
+              variant="outline"
+              className='font-montserratMedium w-full lg:ml-2  border-2 dark:border-gray-800 px-2 bg-white/70 dark:bg-gray-700/70'
+            />
+          </div>
+          <div className="lg:flex lg:space-y-0 space-y-4 mt-4">
+            <Input
+              name="email"
+              value={formData.email}
+              onChange={handleInputChange}
+              placeholder="Email"
+              variant="outline"
+              className='font-montserratMedium w-full lg:mr-2 border-2 dark:border-gray-800 px-2 bg-white/70 dark:bg-gray-700/70'
+            />
+            <Input
+              name="subject"
+              value={formData.subject}
+              onChange={handleInputChange}
+              placeholder="Subject"
+              variant="outline"
+              className='font-montserratMedium w-full lg:ml-2 border-2 dark:border-gray-800 px-2 bg-white/70 dark:bg-gray-700/70'
+            />
+          </div>
+          <div className='mt-4'>
+            <Textarea
+              name="message"
+              value={formData.message}
+              onChange={handleInputChange}
+              placeholder="Message"
+              variant="outline"
+              className='font-montserratMedium h-48 w-full border-2 dark:border-gray-800 p-2 bg-white/70 dark:bg-gray-700/70'
+            />
+          </div>
+          <div className='flex justify-center'>
+            <Button
+              type="submit"
+              variant="solid"
+              className='transform transition-all duration-200 ease-in-out active:translate-y-1 active:scale-95 hover:scale-105 hover:shadow-xl border-2 dark:border-gray-800 font-montserratMedium mt-4 bg-offwhite dark:bg-gray-800 text-gray-600 dark:text-gray-200 p-4'
+            >
+              {submitted ? (
+                <>
+                  <RiCheckLine /> Sent!
+                </>
+              ) : (
+                <>
+                  <RiMailLine /> Send
+                </>
+              )}
+            </Button>
+          </div>
+        </form>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
