e44953a897106d9cf707c9bf0c4bf33b8f2e986f
Alex Manning <alex.t.manning@gmail.com>
2026-03-11 22:37:49 +1300

    Remove square bg shadow from sounf info icon

--- src/components/SoundInfo.jsx
+++ src/components/SoundInfo.jsx
@@ -25,7 +25,7 @@ const SoundInfo = () => {
         <div>
             <PopoverRoot size={'xs'} open={open} onOpenChange={(e) => setOpen(e.open)}>
                 <PopoverTrigger asChild>
-                    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30" height="30" viewBox="0 0 24 24" fill="#374151" className='ml-2 hover:cursor-pointer transform transition-all duration-200 ease-in-out active:translate-y-1 active:scale-95 hover:scale-105 hover:shadow-xl z-50'>
+                    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30" height="30" viewBox="0 0 24 24" fill="#374151" className='ml-2 hover:cursor-pointer transform transition-all duration-200 ease-in-out active:translate-y-1 active:scale-95 hover:scale-105'>
                         <path d="M 12 2 C 6.4889971 2 2 6.4889971 2 12 C 2 17.511003 6.4889971 22 12 22 C 17.511003 22 22 17.511003 22 12 C 22 6.4889971 17.511003 2 12 2 z M 12 4 C 16.430123 4 20 7.5698774 20 12 C 20 16.430123 16.430123 20 12 20 C 7.5698774 20 4 16.430123 4 12 C 4 7.5698774 7.5698774 4 12 4 z M 11 7 L 11 9 L 13 9 L 13 7 L 11 7 z M 11 11 L 11 17 L 13 17 L 13 11 L 11 11 z"></path>
                     </svg>
                 </PopoverTrigger>
